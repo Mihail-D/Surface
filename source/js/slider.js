4d2189d2793285dc7ff44c2e05add3932fa29f63
Mihail-D <nuby.4register@gmail.com>
2020-07-16 09:19:18 +0300

    +

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,22 +1,35 @@
 /*global $*/
 "use strict";
 
-$(document).ready(function() {
-	$(".slider").slick({
-		dots: true,
-		adaptiveHeight: true,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		speed: 1000,
-		easing: "linear",
-		infinite: true,
-		initialSlide: 0,
-		autoplay: true,
-		autoplaySpeed: 5000,
-		pauseOnFocus: true,
-		pauseOnHover: true,
-		pauseOnDotsHover: true,
-		draggble: false,
-		waitForAnimate: false,
-	});
+$(document).ready(function () {
+	let sliderOne = function () {
+		$(".slider").slick({
+			dots: true,
+			arrows: false,
+			adaptiveHeight: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			speed: 1000,
+			easing: "linear",
+			infinite: true,
+			initialSlide: 0,
+			autoplay: true,
+			autoplaySpeed: 1000,
+			pauseOnFocus: true,
+			pauseOnHover: true,
+			pauseOnDotsHover: true,
+			draggble: false,
+			waitForAnimate: false,
+		});
+	};
+	// $(window).on("load resize", function () {
+	// 	let withWindow = window.innerWidth;
+	// 	if (withWindow < 768) {
+	// 		sliderOne();
+	// 	} else {
+	// 		$(".slider").slick("unslick");
+	// 	}
+	// });
+	sliderOne();
 });
+
